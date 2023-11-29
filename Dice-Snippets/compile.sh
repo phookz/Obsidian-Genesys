682da78ea8813f0678fe3d943085e6bcbecffc56
Dwayne Davis <8833993+phookz@users.noreply.github.com>
2023-11-29 15:38:37 -0800

    Added StarWars-Alt.css and source files to more closely match source material dice symbols

--- Dice-Snippets/compile.sh
+++ Dice-Snippets/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sass --watch src/genesys.scss snippets/GenesysDice/GenesysDice.css & 
-sass --watch src/starwars.scss snippets/StarWarsDice/StarWarsDice.css 
+sass --watch src/starwars.scss snippets/StarWarsDice/StarWarsDice.css &
+sass --watch src/starwars-alt.scss snippets/StarWarsDice/StarWarsDice-Alt.css 
