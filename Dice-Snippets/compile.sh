762e91c2b0873cc159c20a7c1e56a7da7d7fdf01
phookz <8833993+phookz@users.noreply.github.com>
2026-01-16 10:54:59 -0800

    Refactored scss to be more robust.

--- Dice-Snippets/compile.sh
+++ Dice-Snippets/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sass --watch src/genesys.scss snippets/GenesysDice/GenesysDice.css & 
-sass --watch src/starwars.scss snippets/StarWarsDice/StarWarsDice.css &
-sass --watch src/starwars-alt.scss snippets/StarWarsDice/StarWarsDice-Alt.css 
+sass --watch src/scss/genesys.scss snippets/GenesysDice/GenesysDice.css & 
+sass --watch src/scss/starwars.scss snippets/StarWarsDice/StarWarsDice.css &
+sass --watch src/scss/starwars-alt.scss snippets/StarWarsDice/StarWarsDice-Alt.css 
